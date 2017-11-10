72f2026927a8b55bc50ed187b49743e7b92b9c13
deicidelove <deicidelove@sina.com>
2017-11-10 14:21:16 +0800

    taowang
    tiiao

--- blue-web/src/main/java/com/common/system/config/WechatMpConfiguration.java
+++ blue-web/src/main/java/com/common/system/config/WechatMpConfiguration.java
@@ -1,12 +1,28 @@
 package com.common.system.config;
 
+import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.mp.api.WxMpConfigStorage;
 import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.constant.WxMpEventConstants;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.common.system.handler.AbstractHandler;
+import com.common.system.handler.KfSessionHandler;
+import com.common.system.handler.LocationHandler;
+import com.common.system.handler.LogHandler;
+import com.common.system.handler.MenuHandler;
+import com.common.system.handler.MsgHandler;
+import com.common.system.handler.NullHandler;
+import com.common.system.handler.StoreCheckNotifyHandler;
+import com.common.system.handler.SubscribeHandler;
+import com.common.system.handler.UnsubscribeHandler;
+
 
 /**
  * wechat mp configuration
@@ -19,6 +35,25 @@ public class WechatMpConfiguration {
     @Autowired
     private WechatMpProperties properties;
 
+    @Autowired
+    protected LogHandler logHandler;
+    @Autowired
+    protected NullHandler nullHandler;
+    @Autowired
+    protected KfSessionHandler kfSessionHandler;
+    @Autowired
+    protected StoreCheckNotifyHandler storeCheckNotifyHandler;
+    @Autowired
+    private LocationHandler locationHandler;
+    @Autowired
+    private MenuHandler menuHandler;
+    @Autowired
+    private MsgHandler msgHandler;
+    @Autowired
+    private UnsubscribeHandler unsubscribeHandler;
+    @Autowired
+    private SubscribeHandler subscribeHandler;
+    
     @Bean
     @ConditionalOnMissingBean
     public WxMpConfigStorage configStorage() {
@@ -40,6 +75,88 @@ public class WechatMpConfiguration {
         wxMpService.setWxMpConfigStorage(configStorage);
         return wxMpService;
     }
+    @Bean
+    public WxMpMessageRouter router(WxMpService wxMpService) {
+        final WxMpMessageRouter newRouter = new WxMpMessageRouter(wxMpService);
 
+        // 记录所有事件的日志 （异步执行）
+        newRouter.rule().handler(this.logHandler).next();
 
+        // 接收客服会话管理事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxMpEventConstants.CustomerService.KF_CREATE_SESSION)
+                .handler(this.kfSessionHandler).end();
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxMpEventConstants.CustomerService.KF_CLOSE_SESSION)
+                .handler(this.kfSessionHandler)
+                .end();
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxMpEventConstants.CustomerService.KF_SWITCH_SESSION)
+                .handler(this.kfSessionHandler).end();
+
+        // 门店审核事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxMpEventConstants.POI_CHECK_NOTIFY)
+                .handler(this.storeCheckNotifyHandler).end();
+
+        // 自定义菜单事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxConsts.BUTTON_CLICK).handler(this.getMenuHandler()).end();
+
+        // 点击菜单连接事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxConsts.BUTTON_VIEW).handler(this.nullHandler).end();
+
+        // 关注事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxConsts.EVT_SUBSCRIBE).handler(this.getSubscribeHandler())
+                .end();
+
+        // 取消关注事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxConsts.EVT_UNSUBSCRIBE)
+                .handler(this.getUnsubscribeHandler()).end();
+
+        // 上报地理位置事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxConsts.EVT_LOCATION).handler(this.getLocationHandler())
+                .end();
+
+        // 接收地理位置消息
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_LOCATION)
+                .handler(this.getLocationHandler()).end();
+
+        // 扫码事件
+        newRouter.rule().async(false).msgType(WxConsts.XML_MSG_EVENT)
+                .event(WxConsts.EVT_SCAN).handler(this.getScanHandler()).end();
+
+        // 默认
+        newRouter.rule().async(false).handler(this.getMsgHandler()).end();
+
+        return newRouter;
+    }
+
+    protected MenuHandler getMenuHandler() {
+        return this.menuHandler;
+    }
+
+    protected SubscribeHandler getSubscribeHandler() {
+        return this.subscribeHandler;
+    }
+
+    protected UnsubscribeHandler getUnsubscribeHandler() {
+        return this.unsubscribeHandler;
+    }
+
+    protected AbstractHandler getLocationHandler() {
+        return this.locationHandler;
+    }
+
+    protected MsgHandler getMsgHandler() {
+        return this.msgHandler;
+    }
+
+    protected AbstractHandler getScanHandler() {
+        return null;
+    }
 }
---
+++ blue-web/src/main/java/com/common/system/handler/SubscribeHandler.java
@@ -0,0 +1,95 @@
+package com.common.system.handler;
+
+import com.common.system.builder.TextBuilder;
+import com.common.system.entity.WxDetailEntity;
+import com.common.system.entity.WxUserEntity;
+import com.common.system.service.WxDetailService;
+import com.common.system.service.WxUserService;
+
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+@Component
+public class SubscribeHandler extends AbstractHandler {
+
+	@Resource
+	private WxUserService wxUserService;
+	@Resource
+	private WxDetailService wxDetailService;
+	
+    @Override
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
+                                    Map<String, Object> context, WxMpService weixinService,
+                                    WxSessionManager sessionManager) throws WxErrorException {
+
+        this.logger.info("新关注用户 OPENID: " + wxMessage.getFromUser());
+
+        // 获取微信用户基本信息
+        WxMpUser userWxInfo = weixinService.getUserService()
+                .userInfo(wxMessage.getFromUser(), null);
+
+        if (userWxInfo != null) {
+            // TODO 可以添加关注用户到本地
+        	try {
+        		WxUserEntity wxUserEntity = wxUserService.getById(userWxInfo.getOpenId());
+        		if(null == wxUserEntity){
+        			wxUserEntity = new WxUserEntity();
+        			wxUserEntity.setOpenId(userWxInfo.getOpenId());
+        			wxUserService.save(wxUserEntity);
+        		}
+        		WxDetailEntity wxDetailEntity = wxDetailService.findByOpenId(userWxInfo.getOpenId());
+        		if(null == wxDetailEntity){
+        			wxDetailEntity = new WxDetailEntity();
+        			wxDetailEntity.setOpenId(userWxInfo.getOpenId());
+        			wxDetailEntity.setPic(userWxInfo.getHeadImgUrl());
+        			wxDetailEntity.setSex(userWxInfo.getSex());
+        			wxDetailService.save(wxDetailEntity);
+        		}
+			} catch (Exception e) {
+				this.logger.error("保存用户信息报错！", e);
+			}
+        }
+
+        WxMpXmlOutMessage responseResult = null;
+        try {
+            responseResult = handleSpecial(wxMessage);
+        } catch (Exception e) {
+            this.logger.error(e.getMessage(), e);
+        }
+
+        if (responseResult != null) {
+            return responseResult;
+        }
+
+        try {
+            return new TextBuilder().build("感谢关注", wxMessage, weixinService);
+        } catch (Exception e) {
+            this.logger.error(e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    /**
+     * 处理特殊请求，比如如果是扫码进来的，可以做相应处理
+     */
+    private WxMpXmlOutMessage handleSpecial(WxMpXmlMessage wxMessage)
+            throws Exception {
+        //TODO
+        return null;
+    }
+
+}
