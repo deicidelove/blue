678eecb3dfaa61828cdf569a89c4ed8d9c23d7be
deicidelove <deicidelove@sina.com>
2017-11-13 23:49:41 +0800

    taowang
    集成服务

--- blue-web/src/main/java/com/common/system/Application.java
+++ blue-web/src/main/java/com/common/system/Application.java
@@ -1,8 +1,12 @@
 package com.common.system;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import me.chanjar.weixin.common.bean.menu.WxMenu;
+import me.chanjar.weixin.common.bean.menu.WxMenuButton;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
@@ -12,12 +16,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.common.system.util.CookieUtil;
+import com.google.common.collect.Lists;
 
 @EnableTransactionManagement
 @SpringBootApplication
@@ -46,6 +52,26 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    	
+    	try {
+    		WxMpService wxMpService = ctx.getBean("wxMpService",WxMpService.class);
+    		WxMenu wxMenu = new WxMenu();
+    		List<WxMenuButton> buttons = Lists.newArrayList();
+    		WxMenuButton wxMenuButton = new WxMenuButton();
+    		wxMenuButton.setName("蓝鲟首页");
+    		wxMenuButton.setUrl("http://wx.njlxkq.com/");
+    		wxMenuButton.setKey("lanxun_01");
+    		wxMenuButton.setType("click");
+    		
+    		buttons.add(wxMenuButton);
+    		wxMenu.setButtons(buttons);
+    		wxMpService.getMenuService().menuCreate(wxMenu);
+    		
+		} catch (Exception e) {
+			LOG.error("	创建菜单失败！",e);
+		}
+    	
+    	
     }
 }
